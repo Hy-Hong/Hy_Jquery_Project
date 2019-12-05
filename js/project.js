2a29f5002e5e7b656897574ce7a007097f87cd4b
hy.hong <hy.hong@PNC.LAN>
2019-12-05 20:52:48 +0700

    Update my code

--- js/project.js
+++ js/project.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-
+var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+$(document).ready( () =>{
+    $('#selection').on('change', () => {
+        var recipe = $('#selection').val();
+        selectRecipe(recipe);
+    });
 });
+// Get api [arrow function]
+var getAPI = (api) => {
+    $.ajax({
+        dataType: 'json',
+        url: api,
+        success: (data) => getRecipe(data),
+        error: () => console.error("Cannot request data")
+    });
+}
+// Get recipe [name function]
+function getRecipe(datas){
+    datas.recipe.forEach(el => {
+        // The recipe can get here example: el.name
+        getIngredients(el); // Get all ingredients
+        
+    });
+}
