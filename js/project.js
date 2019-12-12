402dd3e45dc941676f144293d4deac7eaaf5beb0
hy.hong <hy.hong@PNC.LAN>
2019-12-12 19:30:51 +0700

    Update my coding both files

--- js/project.js
+++ js/project.js
@@ -1,24 +1,76 @@
-var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-$(document).ready( () =>{
-    $('#selection').on('change', () => {
-        var recipe = $('#selection').val();
-        selectRecipe(recipe);
-    });
-});
-// Get api [arrow function]
-var getAPI = (api) => {
-    $.ajax({
-        dataType: 'json',
-        url: api,
-        success: (data) => getRecipe(data),
-        error: () => console.error("Cannot request data")
-    });
+function getUrl() {
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    return url;
 }
-// Get recipe [name function]
-function getRecipe(datas){
-    datas.recipe.forEach(el => {
-        // The recipe can get here example: el.name
-        getIngredients(el); // Get all ingredients
-        
-    });
+
+$(document).ready(function(){
+getApi();
+$('#selection').on('change', function(){
+    var recipeId = $('#selection').val();
+    eachRecipe(recipeId);
+})
+});
+
+// Function to getApi
+function getApi(){
+$.ajax({
+    dataType: 'json',
+    url: getUrl(),
+    success: (data) => chooseRecipe(data.recipes),
+    error: () => console.log("Cannot get data"),
+});
+}
+
+var allData = [];
+function chooseRecipe(recipe){
+allData = recipe;
+var option = "";
+recipe.forEach(item => {
+    option += `
+    <option value="${item.id}">${item.name}</option>
+    `;
+});
+$('#selection').append(option);
+}
+
+// function eachRecipe
+function eachRecipe(id){
+allData.forEach(item => {
+    if(item.id == id){
+        //showRecipe()
+        showRecipe(item.name, item.iconUrl, item.nbGuests, item.instructions);
+        //shhowIngredient()
+        showIngredient(item.ingredients);
+        //showStep()....
+    }
+});
+}
+
+//function showRecipe
+function showRecipe(name,img,nbGuests,instructions){
+var result = "";
+result += `
+<img src="${img}" width="100">
+<h2>${name}</h2>
+<h2>${nbGuests}</h2>
+<h2>${instructions}</h2>
+`;
+$('#recipe_result').html(result);
+}
+
+//function showIngredient
+function showIngredient(ing){
+var resultIngredient = "";
+ing.forEach(item => {
+    resultIngredient += `
+    <tr>
+        <td><img src="${item.iconUrl}" width="100"></td>
+        <td><h4>${item.name}</h4></td>
+        <td><h4>${item.quantity}</h4></td>
+        <td><h4>${item.unit}</h4></td>
+    </tr>
+    `;
+});
+$('#recipe_result').append(resultIngredient);
+
 }
